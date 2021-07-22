5872babde4e184ad12dac7a474b83d77aa28985a
Knightofhonour <ongteckliangdavid@gmail.com>
2021-07-22 22:27:23 +0800

    Slack first commit

---
+++ blocks.py
@@ -0,0 +1,74 @@
+onboard_blocks = [
+		{
+			"type": "section",
+            "block_id": "first section",
+			"text": {
+				"type": "mrkdwn",
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*1️⃣ Use the `/suggest` command*. Type `/suggest` to get a list of games or activities to do! Try it out by using the `/suggest` command in this channel."
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "2️⃣ Afraid that the conversation will be too dry? Look out for prompts to keep the ball rolling "
+			}
+		},
+		{
+			"type": "divider"
+		},
+        {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Here are some suggested online games :slightly_smiling_face:!\nClick on the links to try them out:"
+			}
+		},
+		{
+			"type": "section",
+			"fields": [
+				{
+					"type": "mrkdwn",
+					"text": ":red_circle:<fakeLink.toHotelPage.com|agar.io>"
+				},
+				{
+					"type": "mrkdwn",
+					"text": ":large_blue_circle:<fakeLink.toHotelPage.com|skribbl.io>"
+				},
+				{
+					"type": "mrkdwn",
+					"text": ":large_green_circle:<fakeLink.toHotelPage.com|avo>"
+				},
+				{
+					"type": "mrkdwn",
+					"text": ":large_yellow_circle:<fakeLink.toHotelPage.com|Head Ups>"
+				}
+            ]    
+		}
+	]
+
+prompt = [
+        {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Hi, here's a cute little fact: all of you like animals! What a coincidence (or is it hmmmm)"
+			}
+		}
+]
+
+suggest = [
+        {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Hi, here's a cute little fact: all of you like animals! What a coincidence (or is it hmmmm)"
+			}
+		}
+]
